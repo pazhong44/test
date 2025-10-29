d8a8e77f4b254105d7f06c4a1c475ee0cc4d6ea1
Abdul Muqeet Shahzad <F2023332001@umt.edu.pk>
2025-10-29 20:21:03 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Css/Font.Css'
+import Background from "./Component/Background/Background.jsx"
+import Header from "./Component/Header/Header.jsx"
+const App = () => {
+  return (
+  <>
+    <Header/>
+    <Background/>
+  </>
+  )
+}
+
+export default App
