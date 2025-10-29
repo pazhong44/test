bac18d6be009bb305c6ebe17199bec3edfe475e3
Abdul Muqeet Shahzad <F2023332001@umt.edu.pk>
2025-10-29 20:31:38 +0500

    Describe what you changed

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Css/Font.Css'
+import './Css/Font.css'
 import Background from "./src/Component/Background/Background.jsx"
 import Header from "./src/Component/Header/Header.jsx"
 const App = () => {
