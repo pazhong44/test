d543521e1f753c2276133f2b91a4868c05ff1aad
Abdul Muqeet Shahzad <F2023332001@umt.edu.pk>
2025-10-29 20:29:34 +0500

    Describe what you changed

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Css/Font.Css'
+import Background from "./src/Component/Background/Background.jsx"
+import Header from "./src/Component/Header/Header.jsx"
+const App = () => {
+  return (
+  <>
+    <Header/>
+    <Background/>
+  </>
+  )
+}
+
+export default App
